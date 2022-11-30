50e282353c2a3da92b8de560cbb533ff77511cf9
Judith <jbarrilnavarro@gmai.com>
2022-11-30 15:22:20 +0100

    Inicio del proyecto

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+/* contrucción player */
+class Player {
+    constructor(){
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 100
+        this.height = 100
+    }
+    draw() {
+        /* dibujamos objeto 2d */
+        c.fillRect(this.position.x, this.position.x, this.width, this.height)
+    }
+}
+const player = new Player()
+player.draw()
