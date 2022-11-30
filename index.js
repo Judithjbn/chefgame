bf545ec317372cda649b516058ef0b74edafed4a
Judith <jbarrilnavarro@gmai.com>
2022-11-30 17:05:13 +0100

    Creación de personje

--- index.js
+++ index.js
@@ -5,18 +5,37 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 /* contrucción player */
 class Player {
-    constructor(){
+    constructor() {
         this.position = {
             x: 100,
             y: 100
         }
-        this.width = 100
-        this.height = 100
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        
+        this.width = 30
+        this.height = 30
     }
+
     draw() {
         /* dibujamos objeto 2d */
+        c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.x, this.width, this.height)
+    }
+
+    update() {        
+        this.draw()
+        this.position.y += this.velocity.y
     }
 }
 const player = new Player()
-player.draw()
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.update()
+}
+
+animate()
